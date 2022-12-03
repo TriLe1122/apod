c20a90565ed6926d1f0dacd69f0b1ca471ee292f
tri le <letrin93@gmail.com>
2022-12-02 22:09:59 -0700

    set up

---
+++ apod.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'tnl93.us.auth0.com'
+export const audience = 'https://codeworks-demos.com'
+export const clientId = 'tLVX3SDai9H2ouB1sUHwzJStRhNppzMz'
