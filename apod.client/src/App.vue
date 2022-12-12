18b1ec1b319a9a71b0496d2911e217010b35791c
tri le <letrin93@gmail.com>
2022-12-11 19:43:52 -0700

    tri

--- apod.client/src/App.vue
+++ apod.client/src/App.vue
@@ -5,9 +5,9 @@
   <main :style="{ backgroundImage: `url(${apod?.hdurl})` }">
     <router-view />
   </main>
-   <footer class="bg-dark text-light">
+   <!-- <footer class="bg-dark text-light">
     Made with 💖 by CodeWorks
-  </footer>
+  </footer> -->
 </template>
 
 <script>
--- apod.client/src/pages/HomePage.vue
+++ apod.client/src/pages/HomePage.vue
@@ -1,11 +1,20 @@
 <template>
-
+<div class="col-md-6 m-auto text-shadow mt-5">
+  <p class="on-hover">{{apod?.explanation}}</p>
+  <h1>{{apod?.title}}</h1>
+  <h2>{{apod?.date}}</h2>
+</div>
 </template>
 
 <script>
 import { onMounted } from "vue"
+import { AppState } from "../AppState.js"
 import { nasaService } from "../services/NasaService.js"
 import Pop from "../utils/Pop.js"
+import { computed } from '@vue/reactivity';
+
+
+
 
 export default {
   setup() {
@@ -23,7 +32,9 @@ export default {
       getApod()
     })
 
-    return {}
+    return {
+      apod: computed(() => AppState.apod)
+    }
   }
 }
 </script>
